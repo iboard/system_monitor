c12badc09c11d79131f51a392e24bc724664a059
Andi Altendorfer <andi@iboard.cc>
2009-11-01 16:31:00 +0100

    Fixed at server s1.iboard.cc

--- bin/cron_system_monitor.sh
+++ bin/cron_system_monitor.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-DIR=`dirname $0`
+DIR=`dirname $0`/../
 
-echo $DIR
+[ "$1" == "-e" ] && RAILS_ENV=$2
+export RAILS_ENV
+
+if [ -d $DIR ]
+then
+  echo "# EXECUTE system_monior_loop.sh AS $USER IN $DIR RAILS_ENV=$RAILS_ENV" >> /var/log/system_monitor.log
+  START_CD=`pwd`
+  cd $DIR
+    bin/system_monitor_loop.sh $DIR -e $RAILS_ENV >> /var/log/system_monitor.log 2>&1
+  cd $START_CD
+else
+  echo "# ERROR: DIRECTORY '$DIR' NOT EXISTS"
+fi
+
